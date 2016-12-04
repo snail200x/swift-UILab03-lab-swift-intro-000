ce9f7e6e53c8dbcb1b111c81e6630d90c79ed83a
陈松毅 <chensongyi@ppzuche.com>
2016-12-04 21:53:16 +0800

    finish task

--- Cards/ViewController.swift
+++ Cards/ViewController.swift
@@ -12,6 +12,35 @@ class ViewController: UIViewController {
 
     // TODO: Create IB outlets
     
+    @IBOutlet weak var labelUp: UILabel!
+    @IBOutlet weak var labelDown: UILabel!
+    @IBOutlet weak var labelMiddle: UILabel!
+    
+    @IBAction func btnCard1(_ sender: UIButton) {
+        changeCard(cardType: "♣️", cardNumber: "4")
+    }
+    
+    @IBAction func btnCard2(_ sender: UIButton) {
+        changeCard(cardType: "♠️", cardNumber: "3")
+    }
+    @IBAction func btnCard3(_ sender: UIButton) {
+        changeCard(cardType: "♦️", cardNumber: "8")
+    }
+    
+    @IBAction func btnCard4(_ sender: UIButton) {
+        changeCard(cardType: "♥️", cardNumber: "10")
+    }
+    
+    
+    
+    
+    func changeCard(cardType:String, cardNumber: String) {
+        labelUp.text = cardType
+        labelDown.text = cardType
+        labelMiddle.text = cardNumber
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
